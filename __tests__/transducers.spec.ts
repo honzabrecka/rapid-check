874ec46454b393204122fec6bd7c6f3274dce727
Jan Brecka <jan.brecka@gmail.com>
2017-07-11 15:09:57 +0200

    next iteration

---
+++ __tests__/transducers.spec.ts
@@ -0,0 +1,56 @@
+import {
+  AbortableReducer,
+  comp,
+  complement,
+  even,
+  filter,
+  id,
+  inc,
+  map,
+  odd,
+  range,
+  sum,
+  take,
+  tap,
+  transduce,
+} from '../index'
+
+describe('lazy transducers', () => {
+
+  it('even with even number', () => {
+    expect(even(2)).toBe(true)
+  })
+
+  it('even with odd number', () => {
+    expect(even(1)).toBe(false)
+  })
+
+  it('complement', () => {
+    expect(complement(even)(2)).toBe(false)
+  })
+
+  it('comp', () => {
+    expect(comp<number>(inc, inc)(1)).toBe(3)
+  })
+
+  it('range', () => {
+    expect(transduce(id, sum, 0)(range(1, 3))).toBe(6)
+  })
+
+  it('transduce', () => {
+    expect(transduce(map(inc), sum, 0)(range(1, 3))).toBe(9)
+  })
+
+  it('transduce', () => {
+    const xf = comp<AbortableReducer<number, number>>(filter(odd), map(inc))
+    expect(transduce(xf, sum, 0)(range(1, 3))).toBe(6)
+  })
+
+  it('transduce', () => {
+    const mapper = jest.fn()
+    const xf = comp<AbortableReducer<number, number>>(map(tap(mapper)), take(3))
+    expect(transduce(xf, sum, 0)(range(1, 10000))).toBe(6)
+    expect(mapper).toHaveBeenCalledTimes(3)
+  })
+
+})
--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 import * as random from 'random-js'
 
+//-----------------------------
+// rapid
+//-----------------------------
+
 export class RoseTree {
 
   constructor(public root: number, public children: Function) {}
@@ -7,12 +11,12 @@ export class RoseTree {
 }
 
 
-export interface R {
-  integer: (min: number, max: number) => number
+export interface RNG {
+  (min: number, max: number): number
 }
 
 export interface G {
-  (r: R, size: number): RoseTree
+  (rng: RNG, size: number): RoseTree
 }
 
 export const toRoseTrees = (col: any[], f: Function): RoseTree[] =>
@@ -43,8 +47,8 @@ export const shrink = {
   })
 }
 
-const choose = (min: number, max: number, center: number = min): G => (r, _) => {
-  const n = r.integer(min, max)
+const choose = (min: number, max: number, center: number = min): G => (rng, _) => {
+  const n = rng(min, max)
   return new RoseTree(n, () => shrink.int(n, center))
 }
 
@@ -55,11 +59,11 @@ export const gen = {
   int
 }
 
-export function sample(r: R, gen: G, count = 10) {
+export function sample(rng: RNG, gen: G, count = 10) {
   const results = new Array(count)
 
   for (let i = 0; i < count; i++) {
-    results[i] = gen(r, Math.floor(i / 2) + 1)
+    results[i] = gen(rng, Math.floor(i / 2) + 1)
   }
 
   return results
@@ -68,9 +72,7 @@ export function sample(r: R, gen: G, count = 10) {
 const engine = random.engines.mt19937()
 engine.seed(9)
 
-const r = {
-  integer: (min: number, max: number): number => random.integer(min, max)(engine)
-}
+const rng: RNG = (min: number, max: number): number => random.integer(min, max)(engine)
 
 //console.log(sample(r, gen.int, 100))
 
@@ -85,7 +87,7 @@ function shrinkFailing(test: RoseTree, prop: Function) {
 }
 
 function forAll(gen: G, prop: Function) {
-  const samples = sample(r, gen, 100)
+  const samples = sample(rng, gen, 0)
 
   for (let i = 0, v: any; i < samples.length; i++) {
     v = samples[i].root
@@ -101,3 +103,80 @@ function forAll(gen: G, prop: Function) {
 }
 
 forAll(gen.int, (v: number) => v < 40)
+
+
+//-----------------------------
+// fns lib
+//-----------------------------
+
+export type Pair<A, B> = [A, B]
+
+export function* range(min: number, max: number) {
+  for (let i = min; i <= max; i++)
+    yield i
+}
+
+export interface Reducer<A, B> {
+  (prev: B, current: A): B
+}
+
+export type AbortableReducer<A, B> = Reducer<A, Pair<boolean, B>>
+
+export function map<A, B>(f: (v: A) => B) {
+  return (reducer: AbortableReducer<A, B>): AbortableReducer<A, B> =>
+    (prev, current) =>
+      reducer(prev, f(current) as any)// TODODOODOOOOO!!!!!
+}
+
+export function filter<A, B>(f: (v: A) => boolean) {
+  return (reducer: AbortableReducer<A, B>): AbortableReducer<A, B> =>
+    (prev, current) =>
+      f(current) ? reducer(prev, current) : prev
+}
+
+export function take<A, B>(n: number) {
+  return (reducer: AbortableReducer<A, B>): AbortableReducer<A, B> => {
+    let taken = 0
+    return ([_, prev], current) =>
+      reducer([++taken >= n, prev], current)
+  }
+}
+
+function reduce<A, B>(f: AbortableReducer<A, B>, init: B) {
+  return (col: IterableIterator<A>): B => {
+    let current: IteratorResult<A>
+    let reduced: boolean = false
+    let prev = init
+    while (!reduced && !(current = col.next()).done)
+      [reduced, prev] = f([reduced, prev], current.value)
+    return prev
+  }
+}
+
+export function transduce<A, B>(xf: (f: AbortableReducer<A, B>) => AbortableReducer<A, B>, f: Reducer<A, B>, init: B) {
+  const wrap = <A, B>(f: Reducer<A, B>): AbortableReducer<A, B> =>
+    ([reduced, prev], current) => [reduced, f(prev, current)]
+
+  return (col: IterableIterator<A>): B =>
+    reduce(xf(wrap(f)), init)(col)
+}
+
+export const comp = <A>(...fns: Function[]) => (v: A): A =>
+  fns.reduceRight((r, f) => f(r), v)
+
+export const id = <T>(v: T) => v
+
+export const complement = <A>(f: (v: A) => boolean) => (v: A) => !f(v)
+
+export const sum = (a: number, b: number) => a + b
+
+export const inc = (n: number) => n + 1
+
+export const even = (n: number) => n % 2 === 0
+
+export const odd = complement(even)
+
+export const tap = <T>(f: Function) => (v: T) => {
+  f(v)
+  return v
+}
