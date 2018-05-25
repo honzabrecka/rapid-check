a710ce52de821bf487ec71aad114c5c13547951c
Jan Brecka <jan.brecka@gmail.com>
2018-05-25 12:28:31 +0200

    Update jest to 23

--- src/__tests__/shrink.spec.js
+++ src/__tests__/shrink.spec.js
@@ -11,7 +11,7 @@ const gen = require('../generators')
 describe('roundTowardZero', () => {
 
   it('rounds to the smallest integer greater than or equal to a given x where x < 0', () => {
-    expect(roundTowardZero(-0.4)).toBe(0)
+    expect(Math.abs(roundTowardZero(-0.4))).toBe(0)
     expect(roundTowardZero(-2.1)).toBe(-2)
   })
 
